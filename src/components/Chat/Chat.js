c855bdee9ab122ce111eb64ead08b2f17112929a
Vaibhav Isloor <viisloor@gmail.com>
2024-10-07 20:00:00 +0530

    Logic imrpoved

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,33 +1,80 @@
-import react,{useEffect,useState} from "react";
+import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
-import io from 'socket.io-client'
+import io from "socket.io-client";
+
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
+import './Chat.css';
+
+const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
 
 let socket;
 
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
 
-function Chat(){
-    const [name,setName] = useState('');
-    const [room,setRoom] = useState('');
-    const ENDPOINT = 'localhost:5000';
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
 
-    useEffect(()=>{
-        const {name,room} = queryString.parse(location.search)
+    socket = io(ENDPOINT);
 
-        socket = io(ENDPOINT);
+    setRoom(room);
+    setName(name);
 
-        setName(name);
-        setName(room);
+    socket.emit('join', { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
 
-        socket.emit('join',{name,room},()=>{
-        });
+    // Cleanup on component unmount
+    return () => {
+      socket.disconnect();
+      socket.off();
+    };
+  }, [location.search]);
 
-        return ()=>{
-            socket.emit('disconnect');
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
 
-            socket.off();
-        }
-    },[ENDPOINT,location.search]);
-return <h1>Chat</h1>
-}
+    socket.on('roomData', ({ users }) => {
+      setUsers(users);
+    });
+
+    // Cleanup listeners on unmount
+    return () => {
+      socket.off('message');
+      socket.off('roomData');
+    };
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  };
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
+};
 
 export default Chat;
