7435cb5ef4c89da05540d31653f250dfd015c5cc
Vaibhav Isloor <viisloor@gmail.com>
2024-11-06 20:00:00 +0530

    User Functions added

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import { createServer } from "http"; // Import createServer from http
 import { Server } from "socket.io";  // Import Server class from socket.io
 import router from "./Router.js"
+import { addUser, removeUser, getUser, getUsersInRoom } from './Users.js';
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -14,11 +16,20 @@ const io = new Server(server);     // Use new Server for socket.io
 
 
 io.on('connection',(socket)=>{
-    console.log("New COnnection");
+    console.log("New Connection");
+
+    socket.on('join',({name,room},callback)=>{
+        const {error,user} = addUser({id:socket.id,name,room});
+
+        if (error) return callback(error);
+        
+        socket.emit('message',{user: 'admin', text:`${user.name},welcome to the room ${user.room}`})
+        socket.join(user.room);
+});
 
     socket.on('disconnect',()=>{
-        console.log('User Disconnected')
+        console.log('User Disconnected');
     })
-})
+});
 
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
